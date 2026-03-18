bed1890897fd4eff13e98307724e6029da0ee875
d-hels <dh51930@ubt-uni.net>
2026-03-18 22:16:55 +0100

    aded amdin user

---
+++ src/db/migrations/1773868470006_add-admin-user.cjs
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+    pgm.sql(`
+      INSERT INTO users (
+        first_name,
+        last_name,
+        email,
+        password,
+        phone,
+        address,
+        department_id,
+        status,
+        is_active,
+        is_verified,
+        role_id,
+        created_at
+      ) VALUES (
+        'Dielleza',
+        'Helshani',
+        'admin@admin.com',
+        '$2b$10$jHy/7GAMUpfc1iiWjtr6HOfcympGW1l02IVCrwzgNt.b/81RjBRCC',
+        '045 949 101',
+        'Beqe shaqa',
+        '042c7ca1-5e20-493a-9ff9-8fc1bcbb3956',
+        'active',
+        true,
+        true,
+        1,
+        '2026-03-18 19:47:26.714454+00'
+      );
+    `);
+  };
+  
+  exports.down = (pgm) => {
+    pgm.sql(`
+      DELETE FROM users WHERE email = 'admin@admin.com';
+    `);
+  };
